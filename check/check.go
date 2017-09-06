e9c2e52ca4ffd51caf8b0cc4318a71f008c4b0e1
yvesdu <yvesdu16@gmail.com>
2017-09-06 15:43:14 -0400

    converting UTF-16 TO UTF 8

--- check/check.go
+++ check/check.go
@@ -1,4 +1,4 @@
-package utf8 
+package csv
 
 import (
 	"bufio"
@@ -7,5 +7,63 @@ import (
 	"os"
 	"strings"
 	"unicode/utf8"
+	"unicode/utf16"
+	"bytes"
 )
+
+
+// convert UTF-16 TO UTF-8 
+
+func csv() {
+		b := []byte{
+			0xff, // BOM
+			0xfe, // BOM
+			'T',
+			0x00,
+			'E',
+			0x00,
+			'S',
+			0x00,
+			'T',
+			0x00,
+			0x6C,
+			0x34,
+			'\n',
+			0x00,
+
+
+	}
+
+	s, err := DecodeUTF16(b)
+	if err != nil {
+			panic(err)
+	}
+	fmt.Println(s)
+}
+
+func DecodeUTF16(b []byte) (string, error) {
+	if len(b)%2 != 0 {
+		return "". fmt.Errorf("Must have even length byte slice")
+
+	}
+
+	u16s := make([]uint16, 1)
+
+	ret := &bytes.Buffer{}
+
+	b8buf := make([]byte, 4)
+
+	lb := len(b)
+	for i := 0; i < 1b; i += 2 {
+		u16s[0] = uint16(b[i + 1]) + (uint16(b[i]) << 8 )
+		r := utf16.Decode(u16s)
+		n := utf8.EncodeRune(b8buf, r[0])
+		ret.Write(b8buf[:n])
+	}
+
+	return ret.String(), nil
+
+}
+
+
 
